e65aecc2cad67228043203d284ba7b34c0108f77
varneb <bentaviv06@gmail.com>
2023-10-24 02:19:39 +0300

    comment thoughts in main

--- keytransfertestclient/Program.cs
+++ keytransfertestclient/Program.cs
@@ -19,6 +19,13 @@ namespace keytransfertestclient
             TcpClient client = new TcpClient();
             CommHandler communicationhandler = new CommHandler(client);
 //            WinFormHandler formHandler = new WinFormHandler(communicationhandler);
+
+            
+            //GOAL: have a class created here that will be subscribed to the commhandler, and will create the different forms.
+            //this means that the first form will also get created there?
+            //basically the idea is that the class will dispatch the message events to the relevant form. but i dont know if its
+            //even necessary as when i close forms i unsubscribe them from the message event, and yeah definitely 
+            //having a separate class manage what forms to create and when is nice
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1(communicationhandler));
